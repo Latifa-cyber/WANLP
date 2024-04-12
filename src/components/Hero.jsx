4c45a6459107350860ed246f6505e1b2f0b03a1e
Latifa Boudiaf <latifa.boudiaf.7@gmail.com>
2024-04-13 00:38:49 +0100

    hero section responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import img1 from "../assets/home.png";
+import { Link } from "react-router-dom";
 import arrow from "../assets/scroll-arrow.png";
 
 function Hero() {
@@ -9,19 +10,21 @@ function Hero() {
 
   return (
     <div className="my-10">
-      <div className="grid grid-cols-2 gap-1 justify-between px-10 mb-12 items-center relative mx-auto">
+      <div className="md:grid grid-cols-2 md:gap-1 md:justify-between px-10 mb-12 items-center relative mx-auto">
         <div className="md:mr-14">
-          <h1 className="text-5xl font-bold my-10 leading-tight">
+          <h1 className="md:text-5xl text-3xl font-bold md:my-10 my-5 leading-tight">
             مدقق السرقة العلمية تم تحسينه الآن باستخدام كاشف محتوى الذكاء
             الاصطناعي!
           </h1>
-          <p className="text-2xl font-semibold text-gray-500 my-5 max-w-[500px]">
+          <p className="md:text-2xl text-lg font-semibold text-gray-500 my-5 max-w-[500px]">
             فحص السرقة العلمية مع جهاز كشف محتوى متقدم خصيصًا للنصوص العربية،
             مما يضمن الكشف الأكثر دقة عن المحتوى المنسوخ.
           </p>
-          <button className="bg-blue-600 px-10 py-3 rounded-xl text-white font-bold text-xl">
-            تحقق الآن
-          </button>
+          <Link to="/check">
+            <button className="bg-blue-700 px-10 py-3 rounded-xl text-white font-bold md:text-xl hover:shadow-[2px_3px_5px_rgba(0,0,255,0.5)]">
+              تحقق الآن
+            </button>
+          </Link>
         </div>
         <div className="">
           <img className="w-[400px] mx-auto" src={img1} alt="heroImg" />
